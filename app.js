4203938d54d7ecbffd4f622663eeabf10b8a1ae7
Karen Spriggs <k.spriggs01@gmail.com>
2025-06-03 20:15:54 -0500

    connecting to database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import {PORT} from "./config/env.js";
 
+import connectToDatabase from "./database/mongodb.js";
+
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
@@ -16,8 +18,10 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () =>{
+app.listen(PORT, async() =>{
     console.log(`Server started on port ${PORT}`);
+
+    await connectToDatabase();
 });
 
 export default app;
