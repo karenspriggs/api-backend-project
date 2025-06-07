6db6ed7a296cc055a9075d238e866f229748fea0
Karen Spriggs <k.spriggs01@gmail.com>
2025-06-06 22:22:56 -0500

    removing duplicate function from user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -27,21 +27,3 @@ export const getUser = async (req, res, next) => {
         next(error);
     }
 }
-
-export const getUser = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.params.id).select("-password");
-
-        if (!user){
-            const error = new error("User not found");
-
-            error.status = 404;
-
-            throw error;
-        }
-
-        res.status(200).json({success: true, data: user});
-    } catch (error) {
-        next(error);
-    }
-}
