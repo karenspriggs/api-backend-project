0f80c2dc979467fcc216bcca49ddda715b30ba87
Karen Spriggs <k.spriggs01@gmail.com>
2025-06-06 22:21:59 -0500

    authorization and user controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,12 @@
 import {Router} from "express";
+import {getUsers, getUser} from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-    res.send({title: "Get All Users"});
-})
+userRouter.get("/", getUsers);
 
-userRouter.get("/:id", (req, res) => {
-    res.send({title: "Get User Details"});
-})
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => {
     res.send({title: "Make a User"});
